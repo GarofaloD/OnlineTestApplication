b06705a112737814d78e9d6654501ea2a007ae7e
danielgarofalo <garofalodaniel@gmail.com>
2023-02-12 14:13:47 -0600

    Basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WelcomeComponent} from "../components/welcome/welcome.component";
+import {QuestionComponent} from "../components/question/question.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"welcome", component: WelcomeComponent},
+  {path:"question", component: QuestionComponent},
+  {path:"", redirectTo: 'welcome', pathMatch: 'full'},
+  {path:"**", redirectTo: 'welcome', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
